908f5e8baab2d072674c607db999516198a782d1
YuriyMikh <pamukkale1717@gmail.com>
2023-05-04 01:27:02 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './styles.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+
+formRef.addEventListener('submit', onSearch);
+
+function onSearch(event) {
+    event.preventDefault();
+    const searchQuery = event.currentTarget.elements.searchQuery.value;
+  console.log(searchQuery);
+}
+
+function renderMarkupGallery() {}
+
+// Шаблон разметки карточки одного изображения для галереи:
+// <div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>;
