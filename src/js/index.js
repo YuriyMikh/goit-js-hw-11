c4681b14e9846694f7b1e600ac1a2ca8c9f0e745
YuriyMikh <pamukkale1717@gmail.com>
2023-05-11 17:04:58 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,13 @@ const formRef = document.querySelector('#search-form');
 const galleryRef = document.querySelector('.gallery');
 const loadMoreButtonRef = document.querySelector('.load-more');
 
+const modalLightboxGallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+
 const pixabayApiService = new PixabayApiService(); //на основе класса PixabayApiService из файла api-pixabay.js создаем экземпляр класса (со свойствами и методами)
 
-// loadMoreButtonRef.classList.add('is-hidden');
+loadMoreButtonRef.classList.add('is-hidden'); //скрываем кнопку "Load more"
 
 formRef.addEventListener('submit', onSearch);
 loadMoreButtonRef.addEventListener('click', onLoadMore);
@@ -19,28 +23,54 @@ function onSearch(event) {
   event.preventDefault();
 
   pixabayApiService.query = event.currentTarget.elements.searchQuery.value; //записываем термин поиска в свойство searchQuery через геттер и сеттер в файл api-pixabay.js
+  pixabayApiService.resetPage(); //при сабмите формы сбрасываем странички до единицы
 
-  //проверка 
-  pixabayApiService.fetchData().then(({ hits, total }) => {
-    if (hits.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+  //на экземпляре класса pixabayApiService вызываем метод fetchData() из файла api-pixabay.js, цепляем .then() и обрабатываем полученные данные
+  pixabayApiService.fetchData().then(data => {
+    //проверка, если вернулся пустой массив - выводим сообщение о сбое
+    if (data.hits.length === 0) {
+      notificationFailure();
+      clearGalleryContainer();
     }
-  });
 
+    notificationSuccess(data.totalHits);
     clearGalleryContainer();
-  pixabayApiService.resetPage(); //при сабмите формы сбрасываем термин запрос
-  pixabayApiService.fetchData().then(renderMarkup); //на экземпляре класса pixabayApiService вызываем метод fetchData() из файла api-pixabay.js
+    renderMarkup(data);
+    loadMoreButtonRef.classList.remove('is-hidden');
+    console.log(data.totalHits);
+  });
+  // clearGalleryContainer();
+  // pixabayApiService.resetPage(); //при сабмите формы сбрасываем странички до единицы
+  // pixabayApiService.fetchData().then(renderMarkup); //на экземпляре класса pixabayApiService вызываем метод fetchData() из файла api-pixabay.js
 }
 
 function onLoadMore() {
-  pixabayApiService.fetchData().then(renderMarkup);
+  pixabayApiService
+    .fetchData()
+    .then(data => {
+      //проверку переписать!
+      if (!pixabayApiService.hasMorePhotos()) {
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        renderMarkup(data);
+      }
+    })
+    //что вообще пишем в .catch()?
+    .catch(data => {
+      console.log(data);
+      if (data.response.status === 400) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    });
 }
 
-function renderMarkup(photos) {
-  console.log(photos);
-  const markup = photos.hits
+function renderMarkup(data) {
+  console.log(data);
+  const markup = data.hits
     .map(
       element => `
     <a href="${element.largeImageURL}">
@@ -71,6 +101,17 @@ function renderMarkup(photos) {
 //функция для сброса разметки. Будет использоваться при смене термина запроса
 function clearGalleryContainer() {
   galleryRef.innerHTML = '';
+}
+
+//функция для вывода уведомления о сбое
+function notificationFailure() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function notificationSuccess(total) {
+  Notiflix.Notify.success(`Hooray! We found ${total} images.`);
 }
 
 //.then(function (response) {
